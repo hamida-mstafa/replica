728765f7a5a35ca530e1aad8db604faf23b414bd
hamida-mstafa <hamidamstafa@gmail.com>
2018-12-12 17:27:33 +0300

    installed whitenoise and added a link to settings

--- replica/settings.py
+++ replica/settings.py
@@ -149,3 +149,5 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
