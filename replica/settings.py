368b750d9c824100b64b4b6b272c566b832524f2
hamida-mstafa <hamidamstafa@gmail.com>
2018-12-12 17:41:26 +0300

    saving functionality

--- replica/settings.py
+++ replica/settings.py
@@ -146,5 +146,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+django_heroku.settings(locals())
